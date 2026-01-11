80975ab8248c7a7e0e52f295f24fc1e9a3f0942f
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 13:17:13 +0300

    Implement sign-based array segregation logic
    Added source code that dynamically processes an input vector of size 'n' and filters elements into separate positive/negative arrays using conditional logic.

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+         int n,sayi,i,j,k;
+
+    printf("n sayisini giriniz");
+    scanf("%d",&n);
+
+    int pozitif[n];
+    int negatif[n];
+    int pindex=0;
+    int nindex=0;
+
+    for(i=0;i<n;i++){
+        printf("%d. sayyiy girin",i+1);
+        scanf("%d",&sayi);
+
+        if(sayi>0){
+            pozitif[pindex]=sayi;
+            pindex++;
+        }
+        if(sayi<0){
+            negatif[nindex]=sayi;
+            nindex++;
+        }
+    }
+    printf("\n pozitif sayilar: ");
+    for(j=0;j<pindex;j++){
+        printf("%d, ",pozitif[j]);
+    }
+    printf("\n negatif sayilar: ");
+    for(k=0;k<nindex;k++){
+        printf("%d, ",negatif[k]);
+    }
+
+    return 0;
+}
